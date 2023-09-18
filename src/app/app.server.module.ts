5f3eaa80e849d2d8897fd26e91e03b35f362177e
Alex <alex.hawley@vercel.com>
2023-09-18 11:26:41 -0500

    switched api

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -11,7 +11,8 @@ import { provideServer } from '@spartacus/setup/ssr';
   providers: [
     ...provideServer({
       serverRequestOrigin:
-        'https://api.c65i60w-esaustral1-s2-public.model-t.cc.commerce.ondemand.com',
+        // 'https://api.c65i60w-esaustral1-s2-public.model-t.cc.commerce.ondemand.com',
+        'https://api.c3geq3sr3n-uniformsy1-d1-public.model-t.cc.commerce.ondemand.com',
     }),
   ],
 })
--- src/app/spartacus/spartacus-configuration.module.ts
+++ src/app/spartacus/spartacus-configuration.module.ts
@@ -24,7 +24,8 @@ import {
       backend: {
         occ: {
           baseUrl:
-            'https://api.c65i60w-esaustral1-s2-public.model-t.cc.commerce.ondemand.com',
+            // 'https://api.c65i60w-esaustral1-s2-public.model-t.cc.commerce.ondemand.com',
+            'https://api.c3geq3sr3n-uniformsy1-d1-public.model-t.cc.commerce.ondemand.com',
         },
       },
     }),
